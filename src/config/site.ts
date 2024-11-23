600964d8819d08a951e47b97fd0f7c5b7518024a
Rahul-Sharma03 Rahul <rahulasharma2101@gmail.com>
2024-11-23 20:48:47 +0530

    change meta data

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 const SiteConfig={
-  "title":"Blog website",
+  "title":"linkscribe",
   "description":"A vibrant platform to explore diverse topics, insights, and stories, crafted to inspire and inform readers. Discover fresh perspectives and thought-provoking articles on our blog"
 }
 
